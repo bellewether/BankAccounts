ba8670e9b0da7e5fa9f1889c338e4b70d1245979
Yasmin <yasminor@gmail.com>
2016-08-23 10:56:20 -0700

    .withdraw and .deposit methods updated to meet basic requirements

--- Account.rb
+++ Account.rb
@@ -9,12 +9,22 @@ module Bank
       @current_balance = current_balance
     end
 
-    def withdraw(withdraw_amt)
+    # Create a method to randomly assign an id to an account later
+    # def assign_id()
+    # end
 
+    def withdraw(withdraw_amt)
+      if @current_balance < withdraw_amt
+        puts "WARNING: You cannot withdraw an amount greater than your current balance!"
+      else
+        @current_balance -= withdraw_amt
+      end
+      return @current_balance
     end
 
     def deposit(deposit_amt)
-
+      @current_balance += deposit_amt
+      return @current_balance
     end
   end
 end
