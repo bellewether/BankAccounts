5a98faffa468509b6602cfe7d4faf824afd44e83
Yasmin <yasminor@gmail.com>
2016-08-26 14:56:29 -0700

    Added comments

--- Account.rb
+++ Account.rb
@@ -10,19 +10,18 @@ module Bank
 
     def initialize(id, current_balance, date_created)
       @id = id
-      @current_balance = current_balance
-      if current_balance.to_i < self.class::MINIMUM_BALANCE
-        raise ArgumentError.new("You can't do that. You need at least $#{ self.class::MINIMUM_BALANCE } to open an account.")
-      else @current_balance = current_balance
+      if current_balance.to_i < self.class::MINIMUM_BALANCE # An account can't be initialized with a negative amount of money. If user attempts to, an ArgumentError is raised
+        raise ArgumentError.new("You need at least $#{ self.class::MINIMUM_BALANCE } to open an account.")
+      else
+        @current_balance = current_balance
       end
       @date_created = date_created
-      #@owner = owner
     end
 
     def withdraw(withdraw_amt)
       @current_balance -= self.class::WITHDRAWAL_FEE
-      
-      if @current_balance < withdraw_amt + self.class::MINIMUM_BALANCE
+
+      if @current_balance < withdraw_amt + self.class::MINIMUM_BALANCE  # Throw a warning if user attempts to withdraw an amount that will put their account in the negative and prevent the withdrawal
         puts "WARNING: The amount you are trying to withdraw will put your balance below the $#{ self.class::MINIMUM_BALANCE } minimum!"
         @current_balance += self.class::WITHDRAWAL_FEE
       else
@@ -36,10 +35,10 @@ module Bank
       return @current_balance
     end
 
-    def self.all
+    def self.all # This class method reads elements from a CSV file and writes them into an array as a collection of Account instances
       account_array = []
       CSV.read("/Users/yasminor/ada/class-exercises/project-forks/BankAccounts/support/accounts.csv").each_with_index do |line, i|
-        account_array[i] = self.new(line[0], line[1], line[2])
+        account_array[i] = self.new(line[0], line[1], line[2]) #using csv elements and self to create and add new Account instances to an array
       end
       return account_array
     end
@@ -47,24 +46,19 @@ module Bank
     def self.find(id)
       object_array = Bank::Account.all
       object_array.each do |object|
-        if object.id == id
+        if object.id.to_i == id # convert object id from a string to an fixnum as the user is likely to input the id as a fixnum
           return object
         end
       end
     end
-
-    # def add_owner(owner_object)
-    #   @owner = owner_object
-    # end
-
   end
 end
 
 #TEST
-  # my_account = Bank::Account.new(137, 500, "8/24/2016")
-  # puts my_account.withdraw(50)
+# my_account = Bank::Account.new(137, 49, "8/24/2016")
+# puts my_account.withdraw(50)
 #
-# puts Bank::Account.all
+#puts Bank::Account.all
 # puts
 # puts
-# puts Bank::Account.find("15151")
+#puts Bank::Account.find(15151)
